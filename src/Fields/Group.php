48bac23fe930368949f0a854700dbd8e705e2aff
Artiom Neganov <aneganov@gmail.com>
2019-08-10 05:12:08 +0300

    Finished Group field

--- src/Fields/Group.php
+++ src/Fields/Group.php
@@ -7,15 +7,24 @@ use DataMincerCore\Plugin\PluginFieldInterface;
 
 /**
  * @property PluginFieldInterface source
+ * @property string by
  */
 class Group extends PluginFieldBase {
 
-  protected static $pluginId = 'map';
+  protected static $pluginId = 'group';
 
   function getValue($data) {
-    $source = $this->source;
-//    $vals = $this->resolveParams($data, $this->vals);
-//    return array_combine($keys, $vals);
+    $source = $this->source->value($data);
+    $by = $this->resolveParam($data, $this->by);
+    $result = [];
+    foreach ($source as $index => $info) {
+      $key = array_splice($info, $by, 1);
+      if (count($info) == 1) {
+        $info = current($info);
+      }
+      $result[current($key)] = $info;
+    }
+    return $result;
   }
 
   static function getSchemaChildren() {
