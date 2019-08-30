67c345ebe1866eb71da4664d1c15a3a9b09bedc6
Artiom Neganov <aneganov@gmail.com>
2019-08-30 05:16:44 +0300

    Finished PregMatch plugin

--- src/Fields/PregMatch.php
+++ src/Fields/PregMatch.php
@@ -9,6 +9,7 @@ use DataMincerCore\Plugin\PluginFieldInterface;
  * @property PluginFieldInterface regexp
  * @property PluginFieldInterface subject
  * @property int return
+ * @property string variant
  */
 class PregMatch extends PluginFieldBase {
 
@@ -18,11 +19,19 @@ class PregMatch extends PluginFieldBase {
     $regexp = $this->regexp->getValue($data);
     $subject = $this->subject->getValue($data);
     $return = $this->return;
-    $result = NULL;
-    if (preg_match_all($regexp, $subject, $matches)) {
-      if (array_key_exists($return, $matches)) {
-        $result = $matches[$return];
-      }
+    $variant_all = !empty($this->variant) && $this->variant == 'all';
+    $result = $variant_all ? [] : NULL;
+    if ($variant_all) {
+      $res = @preg_match_all($regexp, $subject, $matches);
+    }
+    else {
+      $res = @preg_match($regexp, $subject, $matches);
+    }
+    if ($res === FALSE) {
+      $this->error("Error in the pattern");
+    }
+    else if ($res !== 0 && array_key_exists($return, $matches)) {
+      $result = $matches[$return];
     }
     return $result;
   }
@@ -30,8 +39,9 @@ class PregMatch extends PluginFieldBase {
   static function getSchemaChildren() {
     return parent::getSchemaChildren() + [
       'regexp' => [ '_type' => 'partial', '_required' => TRUE, '_partial' => 'field' ],
+      'variant' => [ '_type' => 'enum', '_required' => FALSE, '_values' => ['all'] ],
       'subject' => [ '_type' => 'partial', '_required' => TRUE, '_partial' => 'field' ],
-      'return' => [ '_type' => 'number', '_required' => FALSE, '_partial' => 'field' ],
+      'return' => [ '_type' => 'number', '_required' => FALSE ],
     ];
   }
 
