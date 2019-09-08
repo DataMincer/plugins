099920bccf82bcbc6973a9feca362757877342e6
Artiom Neganov <aneganov@gmail.com>
2019-09-08 15:12:44 +0300

    GithubUrl plugin: added failsafe attribute

--- src/Fields/GitHubUrl.php
+++ src/Fields/GitHubUrl.php
@@ -4,6 +4,9 @@ namespace DataMincerPlugins\Fields;
 
 use DataMincerCore\Plugin\PluginFieldBase;
 
+/**
+ * @property bool failsafe
+ */
 class GitHubUrl extends PluginFieldBase {
 
   protected static $pluginId = 'githuburl';
@@ -12,7 +15,12 @@ class GitHubUrl extends PluginFieldBase {
     $cmd = 'git config --get remote.origin.url';
     $result = exec($cmd, $output, $return);
     if ($return !== 0) {
-      $this->error("Got error while executing command:\n\t$cmd\n\tReturn code: $return");
+      if (!$this->failsafe) {
+        $this->error("Got error while executing command:\n\t$cmd\n\tReturn code: $return");
+      }
+      else {
+        return "";
+      }
     }
     if (preg_match('~^.*:(\w+/.+)\.git$~', $result, $matches)) {
       $result = 'https://github.com/' . $matches[1];
@@ -20,4 +28,15 @@ class GitHubUrl extends PluginFieldBase {
     return $result;
   }
 
+  static function getSchemaChildren() {
+    return parent::getSchemaChildren() + [
+        'failsafe' => [ '_type' => 'boolean', '_required' => FALSE ],
+      ];
+  }
+
+  static function defaultConfig($data = NULL) {
+    return [
+        'failsafe' => FALSE,
+      ] + parent::defaultConfig($data);
+  }
 }
