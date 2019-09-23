e103c50e4e9b83735b0e81ed23818769e54b12e8
Artiom Neganov <aneganov@gmail.com>
2019-09-23 15:26:24 +0300

    Added Debug field plugin (draft)

---
+++ src/Fields/Debug.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DataMincerPlugins\Fields;
+
+use DataMincerCore\DataMincer;
+use DataMincerCore\Plugin\PluginFieldBase;
+use DataMincerCore\Util;
+use DataMincerPlugins\Traits\DebugTrait;
+
+/**
+ * @property string value
+ * @property bool dense
+ */
+class Debug extends PluginFieldBase {
+
+  use DebugTrait;
+
+  protected static $pluginId = 'debug';
+
+  function getValue($data) {
+    if (!empty($this->select)) {
+      $debug_data = $this->select->getValue($data);
+    }
+    else {
+      $debug_data = $data;
+    }
+    $asd = $this->dense;
+    $debug_data = $this->prepareData($debug_data, $this->dense);
+    DataMincer::logger()->debug(Util::toJson($debug_data));
+  }
+
+}
